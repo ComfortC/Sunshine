0c45d20865058904ed6c8fe87073d8c01c149491
ComfortChinondiwana <comfortchino@gmail.com>
2016-07-03 21:29:05 +0200

    Added Image Utility

--- app/src/main/java/com/example/khumalo/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/khumalo/sunshine/app/DetailActivity.java
@@ -197,7 +197,7 @@ public class DetailActivity extends AppCompatActivity {
                 // Read weather condition ID from cursor
                 int weatherId = data.getInt(COL_WEATHER_CONDITION_ID);
                 // Use placeholder Image
-                mIconView.setImageResource(R.drawable.ic_launcher);
+                mIconView.setImageResource(Utility.getArtResourceForWeatherCondition(weatherId));
 
                 // Read date from cursor and update views for day of week and date
                 long date = data.getLong(COL_WEATHER_DATE);
